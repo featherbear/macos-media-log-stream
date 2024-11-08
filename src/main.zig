f13c10ce479d581fff28fae6dbae79d99d34300b
Andrew Wong <awong6@atlassian.com>
2024-11-08 20:49:59 +1100

    Screen sharing

--- src/main.zig
+++ src/main.zig
@@ -24,8 +24,14 @@ fn read(filter: []const u8) !void {
 
     try stderr.print("Observing events...\n", .{});
 
-    var activeServices = try std.ArrayList([]u8).initCapacity(allocator, 5);
-    defer activeServices.deinit();
+    var activeServices_cam_mic_loc = try std.ArrayList([]u8).initCapacity(allocator, 5);
+    defer activeServices_cam_mic_loc.deinit();
+
+    var activeServices_screen = try std.ArrayList([]u8).initCapacity(allocator, 5);
+    defer activeServices_screen.deinit();
+
+    var activeServices_screen_legacy = try std.ArrayList([]u8).initCapacity(allocator, 5);
+    defer activeServices_screen_legacy.deinit();
 
     var tempServices = try std.ArrayList([]const u8).initCapacity(allocator, 5);
     defer tempServices.deinit();
@@ -35,6 +41,97 @@ fn read(filter: []const u8) !void {
         const parsed = try std.json.parseFromSlice(LogStream, std.heap.page_allocator, buffer[0..bytesRead], .{ .ignore_unknown_fields = true });
         defer parsed.deinit();
 
+        if (std.mem.startsWith(u8, parsed.value.eventMessage, "Content sharing streams ")) {
+            var items = std.mem.splitSequence(u8, parsed.value.eventMessage, "\n");
+            // Skip the first line (text)
+            _ = items.next();
+            while (items.next()) |itemDirty| {
+                const needle = "bundleID: \"";
+
+                const startIdx = std.mem.indexOf(u8, itemDirty, needle).? + needle.len;
+                const endIdx = std.mem.indexOfPos(u8, itemDirty, startIdx, "\"").?;
+
+                const item = itemDirty[startIdx..endIdx];
+                try tempServices.append(item);
+                // try stdout.print(">{s}\n", .{item});
+                // try tempServices
+            }
+
+            {
+                for (tempServices.items) |value| {
+                    if (for (activeServices_screen.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("New service screen:{s}\n", .{value});
+                        var str = try allocator.alloc(u8, value.len);
+                        for (value, 0..) |char, i| {
+                            str[i] = char;
+                        }
+
+                        try activeServices_screen.append(str);
+                    }
+                }
+            }
+
+            // Check for removed services
+            {
+                for (activeServices_screen.items, 0..) |value, idx| {
+                    if (for (tempServices.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("Expiring service screen:{s}\n", .{value});
+                        const removedItem = activeServices_screen.swapRemove(idx); // orderedRemove?
+                        allocator.free(removedItem);
+                    }
+                }
+            }
+        } else if (std.mem.startsWith(u8, parsed.value.eventMessage, "Legacy sharing bundle ids ")) {
+            var items = std.mem.splitSequence(u8, parsed.value.eventMessage, "\n");
+            // Skip the first line (text)
+            _ = items.next();
+            while (items.next()) |item| {
+                // try stdout.print(">{s}\n", .{item});
+                try tempServices.append(item);
+            }
+
+            {
+                for (tempServices.items) |value| {
+                    if (for (activeServices_screen_legacy.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("New service screen:{s}\n", .{value});
+                        var str = try allocator.alloc(u8, value.len);
+                        for (value, 0..) |char, i| {
+                            str[i] = char;
+                        }
+
+                        try activeServices_screen_legacy.append(str);
+                    }
+                }
+            }
+
+            // Check for removed services
+            {
+                for (activeServices_screen_legacy.items, 0..) |value, idx| {
+                    if (for (tempServices.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("Expiring service screen:{s}\n", .{value});
+                        const removedItem = activeServices_screen_legacy.swapRemove(idx); // orderedRemove?
+                        allocator.free(removedItem);
+                    }
+                }
+            }
+        }
+
         if (std.mem.startsWith(u8, parsed.value.eventMessage, "Active ")) {
             const prefix = "Active activity attributions changed to [";
             const extracted = parsed.value.eventMessage[prefix.len .. parsed.value.eventMessage.len - 1];
@@ -42,22 +139,21 @@ fn read(filter: []const u8) !void {
                 try stderr.print("No items in active list\n", .{});
                 // todo remove all from active list
                 continue;
-            }
-
-            var items = std.mem.splitSequence(u8, extracted, ", ");
-
-            while (items.next()) |itemDirty| {
-                const item = itemDirty[1 .. itemDirty.len - 1];
-                if (std.mem.eql(u8, item, "loc:System Services")) {
-                    continue;
+            } else {
+                var items = std.mem.splitSequence(u8, extracted, ", ");
+                while (items.next()) |itemDirty| {
+                    const item = itemDirty[1 .. itemDirty.len - 1];
+                    if (std.mem.eql(u8, item, "loc:System Services")) {
+                        continue;
+                    }
+                    try tempServices.append(item);
                 }
-                try tempServices.append(item);
             }
 
             // Check for new services
             {
                 for (tempServices.items) |value| {
-                    if (for (activeServices.items) |valueB| {
+                    if (for (activeServices_cam_mic_loc.items) |valueB| {
                         if (std.mem.eql(u8, value, valueB)) {
                             break false;
                         }
@@ -68,21 +164,21 @@ fn read(filter: []const u8) !void {
                             str[i] = char;
                         }
 
-                        try activeServices.append(str);
+                        try activeServices_cam_mic_loc.append(str);
                     }
                 }
             }
 
             // Check for removed services
             {
-                for (activeServices.items, 0..) |value, idx| {
+                for (activeServices_cam_mic_loc.items, 0..) |value, idx| {
                     if (for (tempServices.items) |valueB| {
                         if (std.mem.eql(u8, value, valueB)) {
                             break false;
                         }
                     } else true) {
                         try stdout.print("Expiring service {s}\n", .{value});
-                        const removedItem = activeServices.swapRemove(idx); // orderedRemove?
+                        const removedItem = activeServices_cam_mic_loc.swapRemove(idx); // orderedRemove?
                         allocator.free(removedItem);
                     }
                 }
@@ -94,5 +190,6 @@ fn read(filter: []const u8) !void {
 }
 
 pub fn main() !void {
-    try read("subsystem == 'com.apple.controlcenter' && category == 'sensor-indicators' && formatString BEGINSWITH 'Active '");
+    // try read("subsystem == 'com.apple.controlcenter' && category == 'sensor-indicators' && formatString BEGINSWITH 'Active '");
+    try read("subsystem == 'com.apple.controlcenter' && category == 'contentSharing'");
 }
