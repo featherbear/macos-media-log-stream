20dd2636daa5e0c2fc0802753f8abbb615bf3f9e
Andrew Wong <awong6@atlassian.com>
2024-11-08 23:16:28 +1100

    Refactor

--- src/main.zig
+++ src/main.zig
@@ -5,10 +5,153 @@ const LogStream = struct { eventMessage: []const u8, timestamp: []const u8 };
 
 const allocator = std.heap.page_allocator;
 
-fn read(filter: []const u8) !void {
-    const stdout = std.io.getStdOut().writer();
-    const stderr = std.io.getStdErr().writer();
+const stdout = std.io.getStdOut().writer();
+const stderr = std.io.getStdErr().writer();
 
+fn processScreenShare(entry: LogStream, temp: *std.ArrayList([]const u8), activeServices: *std.ArrayList([]u8)) !void {
+    var items = std.mem.splitSequence(u8, entry.eventMessage, "\n");
+    // Skip the first line (text)
+    _ = items.next();
+    while (items.next()) |itemDirty| {
+        const needle = "bundleID: \"";
+
+        const startIdx = std.mem.indexOf(u8, itemDirty, needle).? + needle.len;
+        const endIdx = std.mem.indexOfPos(u8, itemDirty, startIdx, "\"").?;
+
+        const item = itemDirty[startIdx..endIdx];
+        try temp.append(item);
+    }
+
+    {
+        for (temp.items) |value| {
+            if (for (activeServices.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("New service screen:{s}\n", .{value});
+                var str = try allocator.alloc(u8, value.len);
+                for (value, 0..) |char, i| {
+                    str[i] = char;
+                }
+
+                try activeServices.append(str);
+            }
+        }
+    }
+
+    // Check for removed services
+    {
+        for (activeServices.items, 0..) |value, idx| {
+            if (for (temp.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("Expiring service screen:{s}\n", .{value});
+                const removedItem = activeServices.swapRemove(idx); // orderedRemove?
+                allocator.free(removedItem);
+            }
+        }
+    }
+}
+
+fn processScreenShareLegacy(entry: LogStream, temp: *std.ArrayList([]const u8), activeServices: *std.ArrayList([]u8)) !void {
+    var items = std.mem.splitSequence(u8, entry.eventMessage, "\n");
+    // Skip the first line (text)
+    _ = items.next();
+    while (items.next()) |item| {
+        try temp.append(item);
+    }
+
+    {
+        for (temp.items) |value| {
+            if (for (activeServices.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("New service screen:{s}\n", .{value});
+                var str = try allocator.alloc(u8, value.len);
+                for (value, 0..) |char, i| {
+                    str[i] = char;
+                }
+
+                try activeServices.append(str);
+            }
+        }
+    }
+
+    // Check for removed services
+    {
+        for (activeServices.items, 0..) |value, idx| {
+            if (for (temp.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("Expiring service screen:{s}\n", .{value});
+                const removedItem = activeServices.swapRemove(idx); // orderedRemove?
+                allocator.free(removedItem);
+            }
+        }
+    }
+}
+
+fn processCamMicLoc(entry: LogStream, temp: *std.ArrayList([]const u8), activeServices: *std.ArrayList([]u8)) !void {
+    const prefix = "Active activity attributions changed to [";
+    const extracted = entry.eventMessage[prefix.len .. entry.eventMessage.len - 1];
+    if (extracted.len == 0) {
+        try stderr.print("No items in active list\n", .{});
+        // todo remove all from active list
+        return;
+    } else {
+        var items = std.mem.splitSequence(u8, extracted, ", ");
+        while (items.next()) |itemDirty| {
+            const item = itemDirty[1 .. itemDirty.len - 1];
+            if (std.mem.eql(u8, item, "loc:System Services")) {
+                continue;
+            }
+            try temp.append(item);
+        }
+    }
+
+    // Check for new services
+    {
+        for (temp.items) |value| {
+            if (for (activeServices.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("New service {s}\n", .{value});
+                var str = try allocator.alloc(u8, value.len);
+                for (value, 0..) |char, i| {
+                    str[i] = char;
+                }
+
+                try activeServices.append(str);
+            }
+        }
+    }
+
+    // Check for removed services
+    {
+        for (activeServices.items, 0..) |value, idx| {
+            if (for (temp.items) |valueB| {
+                if (std.mem.eql(u8, value, valueB)) {
+                    break false;
+                }
+            } else true) {
+                try stdout.print("Expiring service {s}\n", .{value});
+                const removedItem = activeServices.swapRemove(idx); // orderedRemove?
+                allocator.free(removedItem);
+            }
+        }
+    }
+}
+
+fn read(filter: []const u8) !void {
     var proc = ChildProcess.init(&[_][]const u8{ "/usr/bin/log", "stream", "--style", "ndjson", "--predicate", filter }, allocator);
     proc.stdout_behavior = ChildProcess.StdIo.Pipe;
     proc.stderr_behavior = ChildProcess.StdIo.Ignore;
@@ -42,147 +185,11 @@ fn read(filter: []const u8) !void {
         defer parsed.deinit();
 
         if (std.mem.startsWith(u8, parsed.value.eventMessage, "Content sharing streams ")) {
-            var items = std.mem.splitSequence(u8, parsed.value.eventMessage, "\n");
-            // Skip the first line (text)
-            _ = items.next();
-            while (items.next()) |itemDirty| {
-                const needle = "bundleID: \"";
-
-                const startIdx = std.mem.indexOf(u8, itemDirty, needle).? + needle.len;
-                const endIdx = std.mem.indexOfPos(u8, itemDirty, startIdx, "\"").?;
-
-                const item = itemDirty[startIdx..endIdx];
-                try tempServices.append(item);
-                // try stdout.print(">{s}\n", .{item});
-                // try tempServices
-            }
-
-            {
-                for (tempServices.items) |value| {
-                    if (for (activeServices_screen.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("New service screen:{s}\n", .{value});
-                        var str = try allocator.alloc(u8, value.len);
-                        for (value, 0..) |char, i| {
-                            str[i] = char;
-                        }
-
-                        try activeServices_screen.append(str);
-                    }
-                }
-            }
-
-            // Check for removed services
-            {
-                for (activeServices_screen.items, 0..) |value, idx| {
-                    if (for (tempServices.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("Expiring service screen:{s}\n", .{value});
-                        const removedItem = activeServices_screen.swapRemove(idx); // orderedRemove?
-                        allocator.free(removedItem);
-                    }
-                }
-            }
+            try processScreenShare(parsed.value, &tempServices, &activeServices_screen);
         } else if (std.mem.startsWith(u8, parsed.value.eventMessage, "Legacy sharing bundle ids ")) {
-            var items = std.mem.splitSequence(u8, parsed.value.eventMessage, "\n");
-            // Skip the first line (text)
-            _ = items.next();
-            while (items.next()) |item| {
-                // try stdout.print(">{s}\n", .{item});
-                try tempServices.append(item);
-            }
-
-            {
-                for (tempServices.items) |value| {
-                    if (for (activeServices_screen_legacy.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("New service screen:{s}\n", .{value});
-                        var str = try allocator.alloc(u8, value.len);
-                        for (value, 0..) |char, i| {
-                            str[i] = char;
-                        }
-
-                        try activeServices_screen_legacy.append(str);
-                    }
-                }
-            }
-
-            // Check for removed services
-            {
-                for (activeServices_screen_legacy.items, 0..) |value, idx| {
-                    if (for (tempServices.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("Expiring service screen:{s}\n", .{value});
-                        const removedItem = activeServices_screen_legacy.swapRemove(idx); // orderedRemove?
-                        allocator.free(removedItem);
-                    }
-                }
-            }
-        }
-
-        if (std.mem.startsWith(u8, parsed.value.eventMessage, "Active ")) {
-            const prefix = "Active activity attributions changed to [";
-            const extracted = parsed.value.eventMessage[prefix.len .. parsed.value.eventMessage.len - 1];
-            if (extracted.len == 0) {
-                try stderr.print("No items in active list\n", .{});
-                // todo remove all from active list
-                continue;
-            } else {
-                var items = std.mem.splitSequence(u8, extracted, ", ");
-                while (items.next()) |itemDirty| {
-                    const item = itemDirty[1 .. itemDirty.len - 1];
-                    if (std.mem.eql(u8, item, "loc:System Services")) {
-                        continue;
-                    }
-                    try tempServices.append(item);
-                }
-            }
-
-            // Check for new services
-            {
-                for (tempServices.items) |value| {
-                    if (for (activeServices_cam_mic_loc.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("New service {s}\n", .{value});
-                        var str = try allocator.alloc(u8, value.len);
-                        for (value, 0..) |char, i| {
-                            str[i] = char;
-                        }
-
-                        try activeServices_cam_mic_loc.append(str);
-                    }
-                }
-            }
-
-            // Check for removed services
-            {
-                for (activeServices_cam_mic_loc.items, 0..) |value, idx| {
-                    if (for (tempServices.items) |valueB| {
-                        if (std.mem.eql(u8, value, valueB)) {
-                            break false;
-                        }
-                    } else true) {
-                        try stdout.print("Expiring service {s}\n", .{value});
-                        const removedItem = activeServices_cam_mic_loc.swapRemove(idx); // orderedRemove?
-                        allocator.free(removedItem);
-                    }
-                }
-            }
+            try processScreenShareLegacy(parsed.value, &tempServices, &activeServices_screen_legacy);
+        } else if (std.mem.startsWith(u8, parsed.value.eventMessage, "Active ")) {
+            try processCamMicLoc(parsed.value, &tempServices, &activeServices_cam_mic_loc);
         }
 
         tempServices.clearAndFree();
@@ -190,6 +197,7 @@ fn read(filter: []const u8) !void {
 }
 
 pub fn main() !void {
+
     // We only see new events
     try read("(subsystem == 'com.apple.controlcenter' && category == 'sensor-indicators' && formatString BEGINSWITH 'Active ') OR (subsystem == 'com.apple.controlcenter' && category == 'contentSharing')");
 }
