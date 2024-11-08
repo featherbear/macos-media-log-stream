9815d1fbc7db0d0aa745e1d9f7abc457799fe564
Andrew Wong <awong6@atlassian.com>
2024-11-08 18:25:37 +1100

    Kinda works

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,98 @@
 const std = @import("std");
+const ChildProcess = std.process.Child;
 
-pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+const LogStream = struct { eventMessage: []const u8, timestamp: []const u8 };
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+const allocator = std.heap.page_allocator;
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+fn read(filter: []const u8) !void {
+    const stdout = std.io.getStdOut().writer();
+    const stderr = std.io.getStdErr().writer();
 
-    try bw.flush(); // don't forget to flush!
+    var proc = ChildProcess.init(&[_][]const u8{ "/usr/bin/log", "stream", "--style", "ndjson", "--predicate", filter }, allocator);
+    proc.stdout_behavior = ChildProcess.StdIo.Pipe;
+    proc.stderr_behavior = ChildProcess.StdIo.Ignore;
+    try proc.spawn();
+
+    // The max I've seen is around 5400 bytes
+    var buffer: [8192]u8 = undefined;
+
+    const reader = proc.stdout.?.reader();
+
+    // Skip the first line: "Filtering the log data using ..."
+    _ = try reader.readUntilDelimiter(&buffer, '\n');
+
+    try stderr.print("Observing events...\n", .{});
+
+    var activeServices = try std.ArrayList([]u8).initCapacity(allocator, 5);
+    defer activeServices.deinit();
+
+    var tempServices = try std.ArrayList([]const u8).initCapacity(allocator, 5);
+    defer tempServices.deinit();
+
+    while (true) {
+        const bytesRead = (try reader.readUntilDelimiter(&buffer, '\n')).len;
+        const parsed = try std.json.parseFromSlice(LogStream, std.heap.page_allocator, buffer[0..bytesRead], .{ .ignore_unknown_fields = true });
+        defer parsed.deinit();
+
+        if (std.mem.startsWith(u8, parsed.value.eventMessage, "Active ")) {
+            const prefix = "Active activity attributions changed to [";
+            const extracted = parsed.value.eventMessage[prefix.len .. parsed.value.eventMessage.len - 1];
+            if (extracted.len == 0) {
+                try stderr.print("No items in active list\n", .{});
+                // todo remove all from active list
+                continue;
+            }
+
+            var items = std.mem.splitSequence(u8, extracted, ", ");
+
+            while (items.next()) |itemDirty| {
+                const item = itemDirty[1 .. itemDirty.len - 1];
+                if (std.mem.eql(u8, item, "loc:System Services")) {
+                    continue;
+                }
+                try tempServices.append(item);
+            }
+
+            // Check for new services
+            {
+                for (tempServices.items) |value| {
+                    if (for (activeServices.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("New service {s}\n", .{value});
+                        var str = try allocator.alloc(u8, value.len);
+                        for (value, 0..) |char, i| {
+                            str[i] = char;
+                        }
+
+                        try activeServices.append(str);
+                    }
+                }
+            }
+
+            // Check for removed services
+            {
+                for (activeServices.items, 0..) |value, idx| {
+                    if (for (tempServices.items) |valueB| {
+                        if (std.mem.eql(u8, value, valueB)) {
+                            break false;
+                        }
+                    } else true) {
+                        try stdout.print("Expiring service {s}\n", .{value});
+                        const removedItem = activeServices.swapRemove(idx); // orderedRemove?
+                        allocator.free(removedItem);
+                    }
+                }
+            }
+        }
+
+        tempServices.clearAndFree();
+    }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+pub fn main() !void {
+    try read("subsystem == 'com.apple.controlcenter' && category == 'sensor-indicators' && formatString BEGINSWITH 'Active '");
 }
