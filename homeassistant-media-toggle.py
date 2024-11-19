a49b3db1a48cc45325352a6661041ae3255236fa
Andrew Wong <andrew.j.wong@outlook.com>
2024-11-19 15:59:09 +1100

    Try catch

--- homeassistant-media-toggle.py
+++ homeassistant-media-toggle.py
@@ -8,10 +8,13 @@ import urllib.request
 import json
 
 def mediaEvent(state: bool):
-    method = 'on' if state else 'off'
-    url = f"{HOMEASSISTANT_BASE_URL}/api/services/light/turn_{method}"
-    req = urllib.request.Request(url, headers={"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}, data=json.dumps({"entity_id": ENTITY_ID}).encode("utf-8"))
-    urllib.request.urlopen(req)
+    try:
+        method = 'on' if state else 'off'
+        url = f"{HOMEASSISTANT_BASE_URL}/api/services/light/turn_{method}"
+        req = urllib.request.Request(url, headers={"Authorization": f"Bearer {TOKEN}", "Content-Type": "application/json"}, data=json.dumps({"entity_id": ENTITY_ID}).encode("utf-8"))
+        urllib.request.urlopen(req)
+    except:
+        print("Failed to send event to Home Assistant")
 
 ###
 
